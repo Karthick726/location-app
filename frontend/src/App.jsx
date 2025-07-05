78f4772ec519d0d513bc7eb8d5713c027cc74f30
Karthick726 <karthickc726@gmail.com>
2025-07-05 11:45:50 +0530

    push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
-
+import axios from 'axios';
 const socket = io("https://location-app-backend-phi.vercel.app");
 
 function App() {
   const [myLocation, setMyLocation] = useState(null);
   const [otherLocation, setOtherLocation] = useState(null);
-
+  const [address, setAddress] = useState('');
  
 
   useEffect(() => {
@@ -20,6 +20,7 @@ function App() {
           };
           setMyLocation(coords);
           socket.emit('send-location', coords);
+          getAddress(coords.lat, coords.lng);
         });
       }
     }, 5000);
@@ -32,11 +33,27 @@ function App() {
     return () => clearInterval(interval);
   }, []);
 
+
+   const getAddress = async (lat, lng) => {
+    try {
+      const apiKey = "cca231bee05d40bba4464e9d62677c5b"; // 🔑 Replace with your key
+      const response = await axios.get(
+        `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lng}&key=${apiKey}`
+      );
+      const result = response.data.results[0];
+      if (result) {
+        setAddress(result.formatted);
+      }
+    } catch (error) {
+      console.error("Error getting address:", error);
+    }
+  };
+
   return (
     <div style={{ padding: '20px' }}>
       <h2>Live Location Sharing (Bus Tracking)</h2>
       <p><strong>Your Location:</strong> {myLocation ? `${myLocation.lat}, ${myLocation.lng}` : 'Fetching...'}</p>
-      <p><strong>Other User:</strong> {otherLocation ? `${otherLocation.lat}, ${otherLocation.lng}` : 'Waiting for data...'}</p>
+    <p><strong>Address:</strong> {address}</p>
     </div>
   );
 }
